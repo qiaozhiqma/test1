72dd00107dc16b7aad24fb098b762ff268d1edfe
qiaozhiqma <47068717+qiaozhiqma@users.noreply.github.com>
2020-04-20 23:03:45 +0800

    ddd

---
+++ fm-education-website/src/test/java/test/Des.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author ：liukun
+ * @date ：Created in 2020-04-20 23:02
+ * @description：des
+ * @modified By：
+ * @version: DES$
+ */
+public class Des {
+}
---
+++ fm-education-website/src/test/java/test/Dev.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @author ：liukun
+ * @date ：Created in 2020-04-20 22:53
+ * @description：dev
+ * @modified By：
+ * @version: dev$
+ */
+public class Dev {
+    public static void main(String[] args) {
+
+    }
+}
